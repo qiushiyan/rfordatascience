ecc9c862d04560404c1f5507f83526af9766f0c2
enixam <565702994@qq.com>
2019-12-12 01:01:18 +0800

    reorganizing chapters

---
+++ _common.R
@@ -0,0 +1,18 @@
+set.seed(1112)
+options(digits = 3)
+
+
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  out.width = "80%",
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  message = F,
+  warning = F
+)
+
+library(tidyverse)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
